01478c5cd74d3ebdbb30a132313e705cc8897183
Daniel Segura <quirkyexplorer@gmail.com>
2026-05-26 22:16:15 -0700

    exercise 1 done

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -10,9 +12,49 @@ package labs_examples.input_output.labs;
  */
 
 class Example {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream input = null;
+        BufferedInputStream bf = null;
+        String path = "src/labs_examples/input_output/files/byte_data";
+        String newPath = "src/labs_examples/input_output/files/newByte_data";
 
 
+        FileOutputStream out = null;
+        BufferedOutputStream bfOut = null;
 
+        try {
+            input  = new FileInputStream(path);
+
+            bf = new BufferedInputStream(input);
+
+            out = new FileOutputStream(newPath);
+
+            bfOut = new BufferedOutputStream(out);
+
+            byte[] buffer = new byte[5];
+
+            int bytesRead = 0;
+
+            while ((bytesRead = bf.read(buffer)) != -1) {
+                   bfOut.write(buffer, 0 , bytesRead);
+            }
+        }
+        catch(IOException e ) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        finally {
+
+            try {
+                bf.close();
+                input.close();
+                bfOut.close();
+                out.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
